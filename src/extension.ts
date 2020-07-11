59374977e37759e60000f42d94fe83574601fa1e
kanako <4190346+mkanako@users.noreply.github.com>
2020-07-11 18:17:34 +0800

    first commit

---
+++ src/extension.ts
@@ -0,0 +1,30 @@
+import {
+  window,
+  workspace,
+  StatusBarAlignment,
+  StatusBarItem,
+} from 'vscode'
+import * as fs from 'fs'
+
+const sizeConvert = (size: number): string => {
+  if (size >= 1048576) return `${Math.floor(size / 10485.76) / 100} MB`
+  else if (size >= 1024) return `${Math.floor(size / 10.24) / 100} KB`
+  else return `${size} B`
+}
+
+function update (bar: StatusBarItem): void {
+  const editor = window.activeTextEditor
+  if (editor && !editor.document.isUntitled && editor.document.uri.scheme === 'file') {
+    bar.text = sizeConvert(fs.statSync(editor.document.fileName).size)
+  } else {
+    bar.text = ''
+  }
+}
+
+export function activate (): void {
+  const bar = window.createStatusBarItem(StatusBarAlignment.Right, 0)
+  update(bar)
+  bar.show()
+  workspace.onDidSaveTextDocument(() => update(bar))
+  window.onDidChangeActiveTextEditor(() => update(bar))
+}
