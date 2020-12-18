0c38634c00a8522150b605483644d57eebd62c17
kanako <4190346+mkanako@users.noreply.github.com>
2020-12-18 22:28:12 +0800

    optimize

--- src/extension.ts
+++ src/extension.ts
@@ -2,23 +2,24 @@ import {
   window,
   workspace,
   StatusBarAlignment,
-  StatusBarItem,
+  commands,
+  TextDocument,
 } from 'vscode'
 import * as fs from 'fs'
 import LRUCache from '@mkanako/lrucache'
 
-const cache = new LRUCache<string>()
+const cache = new LRUCache<string>(30)
 
-const sizeConvert = (size: number): string => {
+function sizeConvert (size: number) {
   if (size >= 1048576) return `${Math.floor(size / 10485.76) / 100} MB`
   else if (size >= 1024) return `${Math.floor(size / 10.24) / 100} KB`
   else return `${size} B`
 }
 
-function update (bar: StatusBarItem, useCache = false): void {
-  const editor = window.activeTextEditor
-  if (editor && !editor.document.isUntitled && editor.document.uri.scheme === 'file') {
-    const path = editor.document.fileName
+function getSize (document?:TextDocument, useCache = false) {
+  document = document || window.activeTextEditor?.document
+  if (document && !document.isUntitled && document.uri.scheme === 'file') {
+    const path = document.fileName
     let text
     if (useCache) {
       text = cache.get(path)
@@ -27,19 +28,38 @@ function update (bar: StatusBarItem, useCache = false): void {
       text = sizeConvert(fs.statSync(path).size)
       cache.set(path, text)
     }
-    bar.text = text
+    return text
   } else {
-    bar.text = ''
+    return ''
   }
 }
 
 export function activate (): void {
   const bar = window.createStatusBarItem(StatusBarAlignment.Right, 0)
-  update(bar)
+  const update = new Proxy(getSize, {
+    apply (target, _, args) {
+      bar.text = target(...args)
+    },
+  })
+  commands.registerCommand('updateSize', () => {
+    update()
+  })
+
+  update()
+
+  bar.command = 'updateSize'
+  bar.tooltip = 'refresh file size'
   bar.show()
-  workspace.onDidSaveTextDocument(() => update(bar))
-  workspace.onDidChangeTextDocument(() => update(bar))
-  window.onDidChangeActiveTextEditor(() => update(bar, true))
+
+  workspace.onDidOpenTextDocument((document) => {
+    update(document)
+  })
+  workspace.onDidSaveTextDocument((document) => {
+    update(document)
+  })
+  window.onDidChangeActiveTextEditor((editor) => {
+    update(editor?.document, true)
+  })
 }
 
 export function deactivate (): void {
